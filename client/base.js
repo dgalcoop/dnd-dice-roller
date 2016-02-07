1c6dff9e273e56745d276058cf6ca37518428b92
Dustin Cooper <dgalcoop@gmail.com>
2016-02-06 19:57:06 -0700

    Adding form for dice creation and addition.

--- client/base.js
+++ client/base.js
@@ -3,17 +3,3 @@ Template.base.helpers({
     return Rolls.find();
   }
 });
-
-Template.base.events({
-	'click .createRoll': function(e) {
-		e.preventDefault();
-
-		Rolls.insert({
-			rollName: 'Warhammer',
-			rollDamage: 'Radiant',
-			diceAmount: 4,
-			diceType: 4,
-			modifier: 3
-		});
-	}
-});
---
+++ client/new_roll.js
@@ -0,0 +1,21 @@
+Template.newRoll.events({
+	'click #createRoll': function(e) {
+		e.preventDefault();
+		var rollName = $('#rollName').val();
+		var diceType = $('#diceType').val();
+		var diceAmount = $('#diceAmount').val();
+		var damageType = $('#damageType').val();
+		var modifier = $('#modifier').val();
+
+
+		Rolls.insert({
+			rollName: rollName,
+			damageType: damageType,
+			diceAmount: diceAmount,
+			diceType: diceType,
+			modifier: modifier
+		});
+		Router.go('/');
+	}
+});
+
